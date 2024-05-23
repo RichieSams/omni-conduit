0856f50e22e53bf45a00fdc1ff1b7d1801dfa282
Adrian Astley <adastley@gmail.com>
2024-05-22 21:11:14 -0400

    Initial setup via fabric template

---
+++ src/client/kotlin/richiesams/omniconduit/OmniconduitClient.kt
@@ -0,0 +1,9 @@
+package richiesams.omniconduit
+
+import net.fabricmc.api.ClientModInitializer
+
+object OmniconduitClient : ClientModInitializer {
+	override fun onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
