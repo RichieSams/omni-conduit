db04a98607ccdafdf601030c1a9b3dca106e51cb
Adrian Astley <adastley@gmail.com>
2024-06-01 11:40:37 -0400

    feat: Play around with using the scroll event
    In preparation for changing yeta wrench modes using shift+scroll

--- src/main/kotlin/richiesams/omniconduit/OmniConduitClientBase.kt
+++ src/main/kotlin/richiesams/omniconduit/OmniConduitClientBase.kt
@@ -3,10 +3,12 @@ package richiesams.omniconduit
 import net.fabricmc.api.ClientModInitializer
 import net.minecraft.client.render.block.entity.BlockEntityRendererFactories
 import richiesams.omniconduit.blockentities.ModBlockEntities
+import richiesams.omniconduit.events.ModClientEvents
 import richiesams.omniconduit.rendering.ConduitBundleBlockEntityRenderer
 
 object OmniConduitClientBase : ClientModInitializer {
     override fun onInitializeClient() {
+        ModClientEvents.registerModClientEvents()
         BlockEntityRendererFactories.register(ModBlockEntities.CONDUIT_BUNDLE, ::ConduitBundleBlockEntityRenderer)
     }
 }
---
+++ src/main/kotlin/richiesams/omniconduit/events/ModClientEvents.kt
@@ -0,0 +1,20 @@
+package richiesams.omniconduit.events
+
+import net.minecraft.client.MinecraftClient
+import net.minecraft.item.Items
+import net.minecraft.text.Text
+
+object ModClientEvents {
+    fun registerModClientEvents() {
+        MouseEvents.MOUSE_WHEEL_SCROLLED.register { dx: Double, dy: Double ->
+            val player = MinecraftClient.getInstance().player ?: return@register false
+
+            if (player.isSneaking && player.getStackInHand(player.activeHand).item == Items.GRASS_BLOCK) {
+                player.sendMessage(Text.literal("Scrolled dx: $dx dy: $dy"))
+                return@register true
+            }
+
+            return@register false
+        }
+    }
+}
